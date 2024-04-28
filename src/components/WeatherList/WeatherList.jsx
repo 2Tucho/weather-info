5969c66a46fdb3fd3ce3eab5b67e915a8577f5f8
2Tucho <albergromero@gmail.com>
2024-04-28 12:35:43 +0200

    add Geolocation for the initial fetch

--- src/components/WeatherList/WeatherList.jsx
+++ src/components/WeatherList/WeatherList.jsx
@@ -3,25 +3,45 @@ import WeatherCard from "./WeatherCard";
 import axios from "axios";
 
 const WeatherList = () => {
-  const [value, setValue] = useState("Madrid"); // Para guardar el dato a buscar
-  const [posts, setPosts] = useState([]); // Para guardar los posts
+  // Cambiar el "Madrid" por la geolocalización
+  const [value, setValue] = useState(""); // Para guardar el dato a buscar
+  const [weatherFetch, setweatherFetch] = useState([]); // Para guardar los weatherFetch
   const inputRef = useRef();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async function getGeolocation(position) {
+      try {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude
+        // Petición HTTP
+        const res = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}&units=metric`);
+        const json = res.data.list;
+        console.log(res.data.city.name)
+        let localCity = res.data.city.name
+
+        setValue(localCity)
+        
+        // Guarda en el array de weatherFetch el resultado. Procesa los datos
+        setweatherFetch(json);
+      } catch (e) {
+        setweatherFetch([]) // No pintes nada 
+      }
+    })
+
+  }, [])
 
   // equivale a un componentDidUpdate()
   useEffect(() => {
     async function fetchData() {
       try {
         // Petición HTTP
-        const fetchUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=${import.meta.env.VITE_API_KEY}&units=metric`
-        console.log(fetchUrl)
-        const res = await axios.get(fetchUrl);
+        const res = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=${import.meta.env.VITE_API_KEY}&units=metric`);
         const json = res.data.list;
-        console.log(json[0].main.temp)
 
-        // Guarda en el array de posts el resultado. Procesa los datos
-        setPosts(json);
+        // Guarda en el array de weatherFetch el resultado. Procesa los datos
+        setweatherFetch(json);
       } catch (e) {
-        setPosts([]) // No pintes nada 
+        setweatherFetch([]) // No pintes nada 
       }
     }
 
@@ -30,11 +50,10 @@ const WeatherList = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(inputRef.current.value)
     setValue(inputRef.current.value) // Modificando el estado de Value
   };
 
-  const paintCards = () => posts.map((forecast, index) => <WeatherCard
+  const paintCards = () => weatherFetch.map((forecast, index) => <WeatherCard
     key={index}
     date={forecast.dt_txt}
     temp={Math.round(forecast.main.temp)}
@@ -42,9 +61,11 @@ const WeatherList = () => {
     icon={forecast.weather[0].icon}
   />);
 
+
   return <section>
     <form onSubmit={handleSubmit}>
-      <input ref={inputRef} />
+      <input ref={inputRef} placeholder="Ciudad" />
+      <button>Buscar</button>
     </form>
     <h3>{value}</h3>
     {paintCards()}
